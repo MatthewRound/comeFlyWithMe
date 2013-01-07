7381a24a7b0b728cfbbb5c15d8400fce03666634
roundyz <roundyz@roundyz.kicks-ass.net>
2013-01-07 22:50:14 +0000

    First working version

--- src/net/kicksAss/roundyz/comeFlyWithMe/ComeFlyWithMeActivity.java
+++ src/net/kicksAss/roundyz/comeFlyWithMe/ComeFlyWithMeActivity.java
@@ -1,18 +1,13 @@
 package net.kicksAss.roundyz.comeFlyWithMe;
 
-import java.io.File;
-
 import android.app.Activity;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class ComeFlyWithMeActivity extends Activity implements OnClickListener 
-{
+public class ComeFlyWithMeActivity extends Activity implements OnClickListener {
 
 	MediaPlayer mp;
 
@@ -26,8 +21,9 @@ public class ComeFlyWithMeActivity extends Activity implements OnClickListener
 	Button weGotSweetner;
 	Button wellTodaySomethingVery;
 
-	public void onCreate(Bundle savedInstanceState) 
-	{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// this.setTitle();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		// views are in main.xml
@@ -52,20 +48,33 @@ public class ComeFlyWithMeActivity extends Activity implements OnClickListener
 		wellTodaySomethingVery.setOnClickListener(this);
 	}
 
-
 	@Override
 	public void onClick(View target) 
 	{
 		// TODO add the others to this
 		if (target == noWater) {
 			this.playAudio(R.raw.but_we_got_no_water);
-			
+		} else if (target == weGotCoffee) {
+			this.playAudio(R.raw.we_got_coffee);
+		}else if (target == weGotCup) {
+			this.playAudio(R.raw.we_got_cup);
+		}else if (target == weGotFullFatMilk){
+			this.playAudio(R.raw.we_got_full_fat_milk);
+		}else if (target == weGotGas){
+			this.playAudio(R.raw.we_got_gas_to_give_us_a_hot_hot_fire);
+		}else if (target ==weGotLowFatMilk){
+			this.playAudio(R.raw.we_got_low_fat_milk);
+		}else if (target ==weGotSoyaMilk){
+			this.playAudio(R.raw.we_got_soya_milk_for_the_lactose_intollerant_community);
+		}else if (target ==weGotSweetner){
+			this.playAudio(R.raw.we_got_sweetner);
+		}else if (target ==wellTodaySomethingVery){
+			this.playAudio(R.raw.well_today_something_very_mysterious_occured);
 		}
 	}
 
-	private void playAudio(Uri wavFileName) 
-	{
-			mp = MediaPlayer.create(this, wavFileName);
-			mp.start();
+	private void playAudio(int wavFileName) {
+		mp = MediaPlayer.create(this, wavFileName);
+		mp.start();
 	}
 }
